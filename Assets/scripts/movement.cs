a3b514b319c4e98c12f88bcd26e8348f9c9e27f0
unknown <roushan_hausil@hotmail.com>
2014-08-26 14:14:17 +0200

    players

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour 
+{
+	public GameObject shadow;
+	void Update() 
+	{
+		if (Input.GetKey(KeyCode.RightArrow)) 
+		{
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+			
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			transform.Translate (-Vector2.right * 4f * Time.deltaTime);
+			
+		}
+
+		if (Input.GetKey(KeyCode.X)) 
+		{
+			shadow.SetActive(true);
+			
+		}
+
+
+	}
+
+}
---
+++ Assets/scripts/shadowMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shadowMovement : MonoBehaviour {
+
+
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.RightArrow)) 
+		{
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+			
+		}
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			transform.Translate (-Vector2.right * 4f * Time.deltaTime);
+			
+		}
+
+		if (Input.GetKey(KeyCode.Z)) 
+		{
+			gameObject.SetActive(false);
+			
+		}
+
+
+	}
+}
